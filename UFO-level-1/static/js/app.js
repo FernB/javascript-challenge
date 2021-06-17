b796b84ba967f0a5908d2df10827e3765009dd4f
FernB <fernbradder@gmail.com>
2021-06-17 15:05:33 +0800

    add comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,57 +3,55 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+// select table 
 var tbody = d3.select("tbody");
 
+// populate table with data
 tableData.forEach((ufoSiting) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoSiting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+  var row = tbody.append("tr");
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
     });
   });
 
-
-
-
-  
+// select filter button
 var button = d3.select("#filter-btn");
 
-
+// select form
 var form = d3.select("form");
 
+// call filter function
 button.on("click", runFilter);
 form.on("submit", runFilter);
 
+// function to filter based on form input
 function runFilter() {
 
-    d3.event.preventDefault();
+  // prevent refresh
+  d3.event.preventDefault();
 
-    tbody.selectAll("tr").remove();
+  // clear all table data
+  tbody.selectAll("tr").remove();
 
-    // Select the input element and get the raw HTML node
-    var filterDates = d3.select("#datetime");
+  // Select the input element and get the raw HTML node
+  var filterDates = d3.select("#datetime");
   
-    // Get the value property of the input element
-    var dateValues = filterDates.property("value");
+  // Get the value property of the input element
+  var dateValues = filterDates.property("value");
   
-
-
-
-     
-    tableData.filter( i => {return (i.datetime === dateValues)}).forEach((ufoSiting) => {
-        var row = tbody.append("tr");
-        Object.entries(ufoSiting).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
+  // populate table with filtered data
+  tableData.filter( i => {
+    return (i.datetime === dateValues)
+    }).forEach((ufoSiting) => {
+      var row = tbody.append("tr");
+      Object.entries(ufoSiting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
         });
       });
       
-
-
-
-
-            }
+  }
 
 
 
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -10,62 +10,67 @@ var tbody = d3.select("tbody");
 // Populate with all data
 
 tableData.forEach((ufoSiting) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoSiting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+  var row = tbody.append("tr");
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
     });
   });
 
 
-  // get list of unique cities 
-  cityList = [];
+// get list of unique cities 
+cityList = [];
   
-
-  tableData.forEach((ufoSiting) => {
-      Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "city" && !cityList.includes(value)) cityList.push(value);});
+tableData.forEach((ufoSiting) => {
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    if(key === "city" && !cityList.includes(value)) 
+    cityList.push(value);
     });
+  });
 
-  cityList.sort();
+cityList.sort();
 
 // set each city as a searchable option
- var cityselect = d3.select("#datalistOptions");
+var cityselect = d3.select("#datalistOptions");
 
- cityList.forEach(i => {
+cityList.forEach(i => {
   var option = cityselect.append("option");
   var optionValue = option.attr("value",i);
-
-
  });
 
 
 // get list of unique states
   
-  stateList = [];
+stateList = [];
   
-  tableData.forEach((ufoSiting) => {
-      Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "state" && !stateList.includes(value)) stateList.push(value);});
+tableData.forEach((ufoSiting) => {
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    if(key === "state" && !stateList.includes(value)) 
+    stateList.push(value);
     });
+  });
   
-  stateList.sort();
+stateList.sort();
 
 // set each state as a dropdown menu option
 var sateselect = d3.select("#state");
   
-  stateList.forEach(i => {
-    var option = sateselect.append("option");
-    var optionValue = option.attr("value",i);
-    optionValue.text(i);
-      });
-
+stateList.forEach(i => {
+  var option = sateselect.append("option");
+  var optionValue = option.attr("value",i);
+  optionValue.text(i);
+  });
 
 // get list of unique countries
 
-  countryList = []
+countryList = []
   
-  tableData.forEach((ufoSiting) => {
-      Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "country" && !countryList.includes(value)) countryList.push(value);});
+tableData.forEach((ufoSiting) => {
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    if(key === "country" && !countryList.includes(value)) 
+    countryList.push(value);
     });
+  });
       
 // set each country as a dropdown option
 var countryselect = d3.select("#country");
@@ -78,39 +83,44 @@ countryList.forEach(i => {
 
 // get list of unique shapes
 shapeList = [];
-shapeList.sort();
-  
+
 tableData.forEach((ufoSiting) => {
-  Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "shape" && !shapeList.includes(value)) shapeList.push(value);});
+  Object.entries(ufoSiting).forEach(([key, value]) => {
+    if(key === "shape" && !shapeList.includes(value)) 
+    shapeList.push(value);
     });
+  });
+
+shapeList.sort();
               
-              
+// set each shape as a dropdown option
 var shapeselect = d3.select("#shape");
               
 shapeList.forEach(i => {
-var option = shapeselect.append("option");
-                  
-var optionValue = option.attr("value",i);
-    optionValue.text(i);
-    });
+  var option = shapeselect.append("option");                 
+  var optionValue = option.attr("value",i);
+  optionValue.text(i);
+  });
                 
   
-  
+// select filter button
 var button = d3.select("#filter-btn");
 
-
+// select form
 var form = d3.select("form");
 
+// call filter function
 button.on("click", runFilter);
 form.on("submit", runFilter);
 
 
-
+// select clear button
 var clearButton = d3.select("#clear-btn");
 
+// call clear cuntions
 clearButton.on("click", runClear);
 
-
+// clear all table filters
 function runClear () {
 
   
@@ -130,71 +140,65 @@ function runClear () {
     });
   });
 
-
 }
 
-// function runFilter() {
+// function to filter table based on form selections
 
 
+function runFilter() {
 
-      function runFilter() {
-
-        d3.event.preventDefault();
-    
-        tbody.selectAll("tr").remove();
-    
-        var filterDates = d3.select("#datetime");
+  // prevent refresh
+  d3.event.preventDefault();
   
-        // Get the value property of the input element
-        var dateValues = filterDates.property("value");
+  // remove all rows
+  tbody.selectAll("tr").remove();
+  
+  // select datetime element
+  var filterDates = d3.select("#datetime");
+  
+  // Get the value property of the input element
+  var dateValues = filterDates.property("value");
     
-        // Select the input element and get the raw HTML node
-        var filterState = d3.select("#state");
+  // Select the input element and get the raw HTML node
+  var filterState = d3.select("#state");
       
-        // Get the value property of the input element
-        var stateValues = filterState.property("value");
+  // Get the value property of the input element
+  var stateValues = filterState.property("value");
       
-        // Select the input element and get the raw HTML node
-        var filterCity = d3.select("#city");
+  // Select the input element and get the raw HTML node
+  var filterCity = d3.select("#city");
       
-        // Get the value property of the input element
-        var cityValues = filterCity.property("value");
+  // Get the value property of the input element
+  var cityValues = filterCity.property("value");
 
-        // Select the input element and get the raw HTML node
-        var filterCountry = d3.select("#country");
+  // Select the input element and get the raw HTML node
+  var filterCountry = d3.select("#country");
       
-        // Get the value property of the input element
-        var countryValues = filterCountry.property("value");
+  // Get the value property of the input element
+  var countryValues = filterCountry.property("value");
 
-        // Select the input element and get the raw HTML node
-        var filterShape = d3.select("#shape");
+  // Select the input element and get the raw HTML node
+  var filterShape = d3.select("#shape");
       
-        // Get the value property of the input element
-        var shapeValues = filterShape.property("value");
+  // Get the value property of the input element
+  var shapeValues = filterShape.property("value");
 
 
-
-    
-    
-         
-        tableData.filter( i => {return (i.datetime === (dateValues ? dateValues : i.datetime)) 
-          && (i.state === (stateValues ? stateValues : i.state)) 
-          && (i.city === (cityValues ? cityValues : i.city)) 
-          && (i.country === (countryValues ? countryValues : i.country)) 
-          && (i.shape === (shapeValues ? shapeValues : i.shape))
-        }).forEach((ufoSiting) => {
-            var row = tbody.append("tr");
-            Object.entries(ufoSiting).forEach(([key, value]) => {
-              var cell = row.append("td");
-              cell.text(value);
+  // Populate table with filter data, ignoring nulls
+  tableData.filter( i => {
+    return (i.datetime === (dateValues ? dateValues : i.datetime)) 
+      && (i.state === (stateValues ? stateValues : i.state)) 
+      && (i.city === (cityValues ? cityValues : i.city)) 
+      && (i.country === (countryValues ? countryValues : i.country)) 
+      && (i.shape === (shapeValues ? shapeValues : i.shape))
+    }).forEach((ufoSiting) => {
+          var row = tbody.append("tr");
+          Object.entries(ufoSiting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
             });
           });
-          
-    
-    
-    
-    
-                }
+  };
     
     
 
