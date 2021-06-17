b080ff6494f119855b83d409009858a16b63b702
FernB <fernbradder@gmail.com>
2021-06-17 13:29:22 +0800

    city autocomplete

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -15,22 +15,36 @@ tableData.forEach((ufoSiting) => {
 
 
 
-  cityList = []
+  cityList = [];
+  cityList.sort();
 
   tableData.forEach((ufoSiting) => {
       Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "city" && !cityList.includes(value)) cityList.push(value);});
     });
   
   console.log(cityList.sort())
+
+ var cityselect = d3.select("#datalistOptions");
+
+ cityList.forEach(i => {
+  var option = cityselect.append("option");
+  var optionValue = option.attr("value",i);
+
+
+ });
+
+
+
   
-  stateList = []
+  stateList = [];
   
   tableData.forEach((ufoSiting) => {
       Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "state" && !stateList.includes(value)) stateList.push(value);});
     });
   
-  
-    var sateselect = d3.select("#state");
+  stateList.sort();
+
+  var sateselect = d3.select("#state");
   
     stateList.forEach(i => {
       var option = sateselect.append("option");
@@ -38,10 +52,45 @@ tableData.forEach((ufoSiting) => {
         var optionValue = option.attr("value",i);
         optionValue.text(i);
       });
-    
+
+
+
+  countryList = []
+  
+  tableData.forEach((ufoSiting) => {
+      Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "country" && !countryList.includes(value)) countryList.push(value);});
+    });
+      
+      
+      var countryselect = d3.select("#country");
+      
+        countryList.forEach(i => {
+          var option = countryselect.append("option");
+          
+            var optionValue = option.attr("value",i);
+            optionValue.text(i);
+          });
+
+
+shapeList = []
+  
+tableData.forEach((ufoSiting) => {
+  Object.entries(ufoSiting).forEach(([key, value]) => {if(key === "shape" && !shapeList.includes(value)) shapeList.push(value);});
+    });
+              
+              
+var shapeselect = d3.select("#shape");
+              
+shapeList.forEach(i => {
+var option = shapeselect.append("option");
+                  
+var optionValue = option.attr("value",i);
+    optionValue.text(i);
+    });
+                
   
   
-  console.log(stateList.sort())
+  
 
 
 //   tableData.forEach((ufoSiting) => {
@@ -59,6 +108,23 @@ var form = d3.select("form");
 
 button.on("click", runFilter);
 form.on("submit", runFilter);
+
+
+
+// var clearButton = d3.select("#clear-btn");
+
+// clearButton.on("click", runClear);
+
+
+// function runClear () {
+
+//   d3.select("#datetime").attr("value",null);
+//   d3.select("#datetime").text("");
+
+
+
+
+// }
 
 // function runFilter() {
 
@@ -125,7 +191,12 @@ form.on("submit", runFilter);
     
     
          
-        tableData.filter( i => {return (i.datetime === (dateValues ? dateValues : i.datetime)) && (i.state === (stateValues ? stateValues : i.state)) && (i.city === (cityValues ? cityValues : i.citytime)) && (i.county === (countryValues ? countryValues : i.country)) && (i.shape === (shapeValues ? shapeValues : i.shape))}).forEach((ufoSiting) => {
+        tableData.filter( i => {return (i.datetime === (dateValues ? dateValues : i.datetime)) 
+          && (i.state === (stateValues ? stateValues : i.state)) 
+          && (i.city === (cityValues ? cityValues : i.city)) 
+          && (i.country === (countryValues ? countryValues : i.country)) 
+          && (i.shape === (shapeValues ? shapeValues : i.shape))
+        }).forEach((ufoSiting) => {
             var row = tbody.append("tr");
             Object.entries(ufoSiting).forEach(([key, value]) => {
               var cell = row.append("td");
